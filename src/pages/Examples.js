b04a091553c89512734a31ceb9f7038001027730
Internship Diggity <internship.diggity@gmail.com>
2024-10-25 00:02:33 +0700

    add InputDate
    menambahkan input date

--- src/pages/Examples.js
+++ src/pages/Examples.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
-import InputNumber from 'elements/Form/inputNumber';
+import { InputDate } from 'elements/Form';
+
+
 
 export default class Example extends Component {
     state = {
-        value: "1"
+        value: {
+            startDate: new Date(),
+            endDate: new Date(),
+            key: "selection",
+          }
     };
 
     handleChange = e => {
@@ -15,14 +21,12 @@ export default class Example extends Component {
             <div className="container">
                 <div className="row align-items-center justify-content-center" style={{height: "100vh"}}>
                     <div className="col-auto">
-                        <InputNumber
-                        max={30}
-                        suffix=' night'
-                        isSuffixPlural
-                        onChange={this.handleChange}
-                        name="value"
-                        value={this.state.value}
-                        />
+                    <InputDate
+        max={30}
+        onChange={this.handleChange}
+        name="value"
+        value={this.state.value}
+      />
                     </div>
                 </div>
             </div>
